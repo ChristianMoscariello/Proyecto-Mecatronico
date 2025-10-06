871950e78965df3982bccc503e1f7e0691bdfbe1
ignaciofm99 <fmalenotti.ignacio@gmail.com>
2025-10-06 17:38:49 -0300

    estable 6-10

--- ESP32/pruebagps1/pruebagps1.ino
+++ ESP32/pruebagps1/pruebagps1.ino
@@ -14,7 +14,7 @@ HardwareSerial gpsSerial(2);
 
 void setup() {
   // Serial Monitor
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   // Start Serial 2 with the defined RX and TX pins and a baud rate of 9600
   gpsSerial.begin(GPS_BAUD, SERIAL_8N1, RXD2, TXD2);
